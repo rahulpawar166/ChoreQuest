a962c3b56aaf24100081ca974b27af895dc6c243
Rahul Pawar <rahul.pawar@eulerity.com>
2026-05-29 14:45:57 -0400

    Initial Commit

---
+++ ChoreQuest/ChoreQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreQuestApp.swift
+//  ChoreQuest
+//
+//  Created by Rahul Pawar on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ChoreQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
